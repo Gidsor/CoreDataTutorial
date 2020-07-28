63282b6a726fce2572d69b398dbac34015ad1d82
Vadim Denisov <gidsor@gmail.com>
2020-07-28 21:38:26 +0500

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Vadim Denisov on 28.07.2020.
+//  Copyright © 2020 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
