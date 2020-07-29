67e3301b37b28e8e4dadfa7e5fde29adf50e6d9f
Vadim Denisov <gidsor@gmail.com>
2020-07-30 01:21:17 +0500

    Complete lesson 2

---
+++ CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Vadim Denisov on 29.07.2020.
+//  Copyright © 2020 Vadim Denisov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
